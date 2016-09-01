b1ac0bb90f5a1d19a796bd69ca34982faae92f19
arlieu <avelieu@gmail.com>
2016-08-31 21:24:12 -0400

    Table formatting and error handling
    'Display all' option completed. Need to implement 'display selected'
    option. Implement 'try-catch' for missing values during data extraction.

--- BasicWebScraper/WebScraper.py
+++ BasicWebScraper/WebScraper.py
@@ -54,8 +54,12 @@ class WebScraper:
                 print("Invalid option.")
                 continue
 
+        if (choice == 'A'): 
+            pass
+
         if (choice == 'B'):
             symbols = list(self.stocks.keys())
+            categories = ["Bid/Ask", "1 Year Price Target", "52 Week High/Low", "Market Cap", "P/E", "EPS", "Beta",]
             bidAskList, targetList, highLowList, capList, peList, epsList, betaList = ([] for i in range(7))
             for company in self.stocks:
                 bid, ask = self.stocks[company]["Bid/Ask"].split('/')
@@ -85,110 +89,143 @@ class WebScraper:
                 beta = "{:.2f}".format(float(self.stocks[company]["Beta"]))
                 betaList.append(beta)
 
-            for sym in symbols: 
-                print(sym, end="") 
+            nameCol = True
+            for sym in symbols:
+                if (nameCol):
+                    print("Statistic", end="")
+                    self.AlignTable("Statistic")
+                print(sym.upper(), end="") 
                 self.AlignTable(sym)
-            self.AlignTable("", True, len(symbols))
+                nameCol = False
+            self.AlignTable("", True, len(symbols)+1)
 
             for i in range(0, len(bidAskList) // len(symbols)):
-                for j in range(0, len(symbols)):
-                    print(bidAskList[i+j], end ="")
-                    self.AlignTable(bidAskList[i+j])
+                for j in range(0, len(symbols)+1):
+                    if (j == 0): 
+                        print(categories[0], end="")
+                        self.AlignTable(categories[0])
+                    else:
+                        print(bidAskList[i+j-1], end ="")
+                        self.AlignTable(bidAskList[i+j-1])
                 print("")                
-                for j in range(0, len(symbols)):
-                    print(targetList[i+j], end ="")
-                    self.AlignTable(targetList[i+j])
+                for j in range(0, len(symbols)+1):
+                    if (j == 0): 
+                        print(categories[1], end="")
+                        self.AlignTable(categories[1])
+                    else:
+                        print(targetList[i+j-1], end ="")
+                        self.AlignTable(targetList[i+j-1])
                 print("")
-                for j in range(0, len(symbols)):
-                    print(highLowList[i+j], end ="")
-                    self.AlignTable(highLowList[i+j])
+                for j in range(0, len(symbols)+1):
+                    if (j == 0): 
+                        print(categories[2], end="")
+                        self.AlignTable(categories[2])
+                    else:
+                        print(highLowList[i+j-1], end ="")
+                        self.AlignTable(highLowList[i+j-1])
                 print("")
-                for j in range(0, len(symbols)):
-                    print(capList[i+j], end ='')
-                    self.AlignTable(capList[i+j])
+                for j in range(0, len(symbols)+1):
+                    if (j == 0): 
+                        print(categories[3], end="")
+                        self.AlignTable(categories[3])
+                    else:
+                        print(capList[i+j-1], end ='')
+                        self.AlignTable(capList[i+j-1])
                 print("")
-                for j in range(0, len(symbols)):
-                    print(peList[i+j], end ="")
-                    self.AlignTable(peList[i+j])
+                for j in range(0, len(symbols)+1):
+                    if (j == 0): 
+                        print(categories[4], end="")
+                        self.AlignTable(categories[4])
+                    else:
+                        print(peList[i+j-1], end ="")
+                        self.AlignTable(peList[i+j-1])
                 print("")
-                for j in range(0, len(symbols)):
-                    print(epsList[i+j], end ="")
-                    self.AlignTable(epsList[i+j])
+                for j in range(0, len(symbols)+1):
+                    if (j == 0): 
+                        print(categories[5], end="")
+                        self.AlignTable(categories[5])
+                    else:
+                        print(epsList[i+j-1], end ="")
+                        self.AlignTable(epsList[i+j-1])
                 print("")
-                for j in range(0, len(symbols)):
-                    print(betaList[i+j], end ="")
-                    self.AlignTable(betaList[i+j])
+                for j in range(0, len(symbols)+1):
+                    if (j == 0): 
+                        print(categories[6], end="")
+                        self.AlignTable(categories[6])
+                    else:
+                        print(betaList[i+j-1], end ="")
+                        self.AlignTable(betaList[i+j-1])
                 print("")
             print("")
 
 
-    def StatOptions(self):
-        '''Options to view saved companies' statistical data.'''
-        targets = []
+    #def StatOptions(self):
+    #    '''Options to view saved companies' statistical data.'''
+    #    targets = []
 
-        print("Statistical Data\n" + \
-            "a) Current Price\n" + \
-            "b) Bid/Ask\n" + \
-            "c) 1 Year Target Price\n" + \
-            "d) 52 Week High/Low\n" + \
-            "e) Market Cap\n" + \
-            "f) P/E\n" + \
-            "g) EPS\n" + \
-            "h) Beta\n")
-        option = input("Please select an option: ").upper()
+    #    print("Statistical Data\n" + \
+    #        "a) Current Price\n" + \
+    #        "b) Bid/Ask\n" + \
+    #        "c) 1 Year Target Price\n" + \
+    #        "d) 52 Week High/Low\n" + \
+    #        "e) Market Cap\n" + \
+    #        "f) P/E\n" + \
+    #        "g) EPS\n" + \
+    #        "h) Beta\n")
+    #    option = input("Please select an option: ").upper()
         
-        print("\nEnter the symbols for the stocks you would like to view.\n" + \
-            "Enter 'end' when all symbols have been input.")
+    #    print("\nEnter the symbols for the stocks you would like to view.\n" + \
+    #        "Enter 'end' when all symbols have been input.")
 
-        n = 1
-        while (1):
-            symbolSelection = input("Symbol " + str(n) + ": ")
-            if (symbolSelection == "end"):
-                if (len(targets) == 0):
-                    print("No symbol entered.")
-                    self.Redirect()
-                else:
-                    break
-            elif (self.SavedSymbol(symbolSelection)):   
-                targets.append(symbolSelection)
-                n += 1
-            else:
-                print("Stock information not saved.")
-                while (1):
-                    addSymbol = input("Would you like to add this symbol (Y/n)?: ").upper()
-                    if (addSymbol == "YES" or addSymbol == "Y"):
-                        self.AddSymbol()
-                    elif (addSymbol == "NO" or addSymbol == "N"):
-                        self.StatOptions()
-                    else:
-                        print("Invalid response.")
-                        continue
+    #    n = 1
+    #    while (1):
+    #        symbolSelection = input("Symbol " + str(n) + ": ")
+    #        if (symbolSelection == "end"):
+    #            if (len(targets) == 0):
+    #                print("No symbol entered.")
+    #                self.Redirect()
+    #            else:
+    #                break
+    #        elif (self.SavedSymbol(symbolSelection)):   
+    #            targets.append(symbolSelection)
+    #            n += 1
+    #        else:
+    #            print("Stock information not saved.")
+    #            while (1):
+    #                addSymbol = input("Would you like to add this symbol (Y/n)?: ").upper()
+    #                if (addSymbol == "YES" or addSymbol == "Y"):
+    #                    self.AddSymbol()
+    #                elif (addSymbol == "NO" or addSymbol == "N"):
+    #                    self.StatOptions()
+    #                else:
+    #                    print("Invalid response.")
+    #                    continue
 
-        if (option == 'A' or option == "CURRENT PRICE"):
-            self.CurrentPrice(self, targets)
+    #    if (option == 'A' or option == "CURRENT PRICE"):
+    #        self.CurrentPrice(self, targets)
 
             
-    def TargetPrice(self, symbol): 
-        if (symbol in self.stocks): 
-            print(self.stocks[symbol]["1 Year Target Price"])
-        else:
-            print("Symbol not in database.")    
-            while (1):
-                addSymbol = input("Would you like to save " + symbol.upper() + "? (Y/n)").upper()
-                if (addSymbol == "YES" or addSymbol == "Y"):
-                    pass        #  restart here
-                elif (addSymbol == "NO" or addSymbol == "N"):
-                    pass        #  send to menu options
-                else:
-                    print("Invalid response.")
-                    continue
+    #def TargetPrice(self, symbol): 
+    #    if (symbol in self.stocks): 
+    #        print(self.stocks[symbol]["1 Year Target Price"])
+    #    else:
+    #        print("Symbol not in database.")    
+    #        while (1):
+    #            addSymbol = input("Would you like to save " + symbol.upper() + "? (Y/n)").upper()
+    #            if (addSymbol == "YES" or addSymbol == "Y"):
+    #                pass        #  restart here
+    #            elif (addSymbol == "NO" or addSymbol == "N"):
+    #                pass        #  send to menu options
+    #            else:
+    #                print("Invalid response.")
+    #                continue
 
     def AddSymbol(self):
         '''Accepts symbols to be added to portfolio.'''
         targets = []
 
         print("Enter the symbols for the stock you are interested in." +
-	    "\nEnter 'end' when all symbols have been input.")
+	    "\nEnter 'end' when all symbols have been input.\n")
 
         n = 0
         end = False
@@ -247,10 +284,10 @@ class WebScraper:
         invalid = soup.find("div", {"class": "notTradingIPO"}) 
 
         if (not invalid):
-            print("Valid")
+            print("Valid\n")
             return True
         else: 
-            print("Invalid symbol")
+            print("Invalid symbol\n")
             return False
 
     def SavedSymbol(self, symbol):
@@ -277,33 +314,48 @@ class WebScraper:
                 continue
     
     def AlignTable(self, value, header=False, n=0):
-        if (len(value) == 3): 
-            print("                |", end="")
-        elif (len(value) == 4):
-            print("               |", end="")
-        elif (len(value) == 5):
-            print("              |", end="")
-        elif (len(value) == 6): 
-            print("             |", end="") 
-        elif (len(value) == 7): 
-            print("            |", end="")  
-        elif (len(value) == 8):
-            print("           |", end="") 
-        elif (len(value) == 9): 
-            print("          |", end="") 
-        elif (len(value) == 10): 
-            print("         |", end="")  
-        elif (len(value) == 11):
-            print("        |", end="") 
-        elif (len(value) == 12):
-            print("       |", end="") 
-        elif (len(value) == 13): 
-            print("      |", end="") 
-        elif (len(value) == 14): 
-            print("     |", end="")  
-        elif (len(value) == 15):
-            print("    |", end="") 
-
-        if (header == True):
+        if (len(value) == 0): 
             print("")
-            print("-------------------|" * n)
+            print("-------------------------|"*n)
+        elif (len(value) == 1):
+            print("                         |", end="")
+        elif (len(value) == 2):
+            print("                       |", end="")
+        elif (len(value) == 3): 
+            print("                      |", end="")
+        elif (len(value) == 4):
+            print("                     |", end="")
+        elif (len(value) == 5):
+            print("                    |", end="")
+        elif (len(value) == 6): 
+            print("                   |", end="") 
+        elif (len(value) == 7): 
+            print("                  |", end="")  
+        elif (len(value) == 8):
+            print("                 |", end="") 
+        elif (len(value) == 9): 
+            print("                |", end="") 
+        elif (len(value) == 10): 
+            print("               |", end="")  
+        elif (len(value) == 11):
+            print("              |", end="") 
+        elif (len(value) == 12):
+            print("             |", end="") 
+        elif (len(value) == 13): 
+            print("            |", end="") 
+        elif (len(value) == 14): 
+            print("           |", end="")  
+        elif (len(value) == 15):
+            print("          |", end="")
+        elif (len(value) == 16): 
+            print("         |", end="")  
+        elif (len(value) == 17):
+            print("        |", end="") 
+        elif (len(value) == 18):
+            print("       |", end="") 
+        elif (len(value) == 19): 
+            print("      |", end="") 
+        elif (len(value) == 20): 
+            print("     |", end="")  
+
+
